87b38183a0efa931e472e51bde9513c29e1d05b2
Martha Huaman <ms.huamann@gmail.com>
2022-10-14 13:49:00 -0500

    CHORE:create controller sheets

---
+++ category.rb
@@ -0,0 +1,10 @@
+class Category < ApplicationRecord
+
+  #VALIDATIONS
+  validates :name, presence: {message: "Name can't be blank"}
+  validates :cover, presence: {message: "Cover can't be blank"}
+
+  #ASSOCIATIONS
+  has_many :pictures, dependent: :destroy
+
+end
